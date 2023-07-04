13d70138b02a189b0a4dd7d89fa190a068688288
Deity <59446525+saint-deity@users.noreply.github.com>
2023-07-05 04:31:03 +1000

    Update RunVRCMixedOSC.bat

--- RunVRCMixedOSC.bat
+++ RunVRCMixedOSC.bat
@@ -1,5 +1,4 @@
 @echo off
-call UpdateScripts.bat
 echo [%~n0] Installing requirements (be sure to have python installed and in PATH)
 python -m pip install -r VRCMixedOSC/requirements.txt
 python VRCMixedOSC/vrcmixedosc.py
